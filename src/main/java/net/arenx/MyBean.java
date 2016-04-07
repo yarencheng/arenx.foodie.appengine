359c45a3aacac96e8e31c6bcb83cfd55d1677a27
arenx <yarencheng@gmail.com>
2016-04-07 23:51:41 +0800

    hello world

---
+++ src/main/java/net/arenx/MyBean.java
@@ -0,0 +1,14 @@
+package net.arenx;
+
+public class MyBean {
+
+	private String myData;
+
+	public String getData() {
+		return myData;
+	}
+
+	public void setData(String data) {
+		myData = data;
+	}
+}
--- src/main/java/net/arenx/YourFirstAPI.java
+++ src/main/java/net/arenx/YourFirstAPI.java
@@ -1,8 +1,25 @@
 package net.arenx;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+
 /**
-  * Add your first API methods in this class, or you may create another class. In that case, please
-  * update your web.xml accordingly.
+ * Add your first API methods in this class, or you may create another class. In
+ * that case, please update your web.xml accordingly.
  **/
+
+@Api(name = "myApi", version = "v1", namespace = @ApiNamespace(ownerDomain = "helloworld.example.com", ownerName = "helloworld.example.com", packagePath = ""))
 public class YourFirstAPI {
+
+	/** A simple endpoint method that takes a name and says Hi back */
+	@ApiMethod(name = "sayHi")
+	public MyBean sayHi(@Named("name") String name) {
+		MyBean response = new MyBean();
+		response.setData("Hi, " + name);
+
+		return response;
+	}
+
 }
